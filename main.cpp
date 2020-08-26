48d34126b4ae728d535b1bc5890dea3033e06104
Shawn Halayka <sjhalayka@gmail.com>
2020-08-26 17:01:29 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -45,30 +45,26 @@ int main(void)
 		vector_4 n_0 = this_tet_centre - tri_0_centre;
 		n_0.normalize();
 
-		vector_4 o_0 = tet_0_centre - tri_0_centre;
+		vector_4 o_0 = tri_0_centre - tet_0_centre;
 		o_0.normalize();
-		o_0 = -o_0;
 
 		vector_4 n_1 = this_tet_centre - tri_1_centre;
 		n_1.normalize();
 
-		vector_4 o_1 = tet_1_centre - tri_1_centre;
+		vector_4 o_1 = tri_1_centre - tet_1_centre;
 		o_1.normalize();
-		o_1 = -o_1;
 
 		vector_4 n_2 = this_tet_centre - tri_2_centre;
 		n_2.normalize();
 
-		vector_4 o_2 = tet_2_centre - tri_2_centre;
+		vector_4 o_2 = tri_2_centre - tet_2_centre;
 		o_2.normalize();
-		o_2 = -o_2;
 
 		vector_4 n_3 = this_tet_centre - tri_3_centre;
 		n_3.normalize();
 
-		vector_4 o_3 = tet_3_centre - tri_3_centre;
+		vector_4 o_3 = tri_3_centre - tet_3_centre;
 		o_3.normalize();
-		o_3 = -o_3;
 
 		double d_i = (n_0.dot(o_0) + n_1.dot(o_1) + n_2.dot(o_2) + n_3.dot(o_3)) / 4.0;
 		double k_i = (1.0 - d_i) / 2.0;
